e28d3c728bf3d263257a3314186016ce9660880f
Nipunjeet Gujral <nipunjeetgujral@gmail.com>
2025-01-05 04:29:22 -0600

    Update postgres_manager.py

--- postgres_manager.py
+++ postgres_manager.py
@@ -1,9 +1,6 @@
-from sqlalchemy import create_engine
-import pandas as pd
 import psycopg2
 
-
-# Create connection methods to PostgresSQL
+# establish postgres connection
 def establish_postgres_connection(database, user, password, host, port):
     """
 	Input:
@@ -13,7 +10,7 @@ def establish_postgres_connection(database, user, password, host, port):
 		host | string | name of the host
 		port | string | port number
 	Output:
-		cursor | psycopg2 cursor | method of connecting to the database
+		cursor | psycopg2 cursor | method of connecting to the database and executing queries
 	"""
 
     try:
@@ -35,65 +32,55 @@ def establish_postgres_connection(database, user, password, host, port):
         print(f"Error connecting to PostgreSQL: {e}")
         return None
 
-def establish_postgres_engine(database, user, password, host, port):
-    """
-	Input:
-		postgres_config | json | all configuration for PostgreSQL
-	Output:
-		engine | str | method of connecting to the database used with pandas
-	"""
-
-    engine = create_engine(f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}")
-
-    return engine
-
-
-# Generate queries
+# generate queries
 def generate_check_database_query(database):
     """
-	Input:
-		database | string | name of the database
-	Output:
-		query | string | SQL query that create a database
-	"""
+    Input:
+        database | string | name of the database
+    Output:
+        query | string | query to execute
+    """
 
     query = f"SELECT * FROM pg_database where datname = '{database}'"
 
     return query
 
+
 def generate_check_table_query(table_name):
     """
-	Input:
-		table_name | string | name of the database
-	Output:
-		query | string | SQL query that create a database
-	"""
+    Input:
+        table_name | string | name of the table_name
+    Output:
+        query | string | query to execute
+    """
 
     query = f"SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = '{table_name}');"
 
     return query
 
+
 def generate_last_record_query(column_of_interest, table_name):
     """
-	Input:
-		table_name | string | name of the table
-		column_of_interest | string | column of interest used to subset the table
-	Output:
-		query | string | SQL query that create a database
-	"""
+    Input:
+        column_of_interest | string | name of the column of interest used to sort the table
+        table_name | string | name of the table_name
+    Output:
+        query | string | query to execute
+    """
 
-    query = f"SELECT {column_of_interest} FROM {table_name} ORDER by {column_of_interest} DESC LIMIT 1"
+    query = f"SELECT * FROM {table_name} ORDER by {column_of_interest} DESC LIMIT 1"
 
     return query
 
+
 def generate_create_table_query(table_schema, table_name):
     """
-	Input:
-		table_schema | json | key value pairing of column name and type
-		table_name | string | name of the table
-	Output:
-		query | string | SQL query that create a table with a particular schema
-	"""
+    Input:
+        table_schema | json | key value pair for all column names and types
+        table_name | string | name of the table_name
+    Output:
+        query | string | query to execute
+    """
 
     column_definitions = []
     for column_name, column_type in table_schema.items():
@@ -105,33 +92,44 @@ def generate_create_table_query(table_schema, table_name):
 
     return query
 
+
 def generate_create_database_query(database):
     """
-	Input:
-		database | string | name of the database
-	Output:
-	 	query | string | SQL query that create a database
-	"""
+    Input:
+        database | string | name of the database
+    Output:
+        query | string | query to execute
+    """
 
-    query = f"""CREATE DATABASE {database}"""
+    query = f"CREATE DATABASE {database}"
 
     return query
 
 
-# Execute boolean return queries
-def check_postgres_database_exists(database, postgres_config):
-
+def generate_insert_query(table_name, column_names, values):
     """
     Input:
-        database | string | name of the database
-        postgres_config | json | method of connecting to the database
+        table_name | string | name of the table_name
+        column_names | list | list of column names
+        values | list | list of row valuest to insert
     Output:
-        exists | bool | True if the database exists
+        query | string | query to execute
     """
 
-    exists = None
+    query = f"INSERT INTO {table_name} ({column_names}) VALUES ({values});"
 
-    engine = establish_postgres_engine(
+    return query
+
+# boolean returns
+def check_database_exists(postgres_config):
+    """
+    Input:
+        postgres_config | json | key value pairs needed to query the specified postgres
+    Output:
+        exists | boolean | status of the existence of the database
+    """
+
+    connection = establish_postgres_connection(
         database="postgres",
         user=postgres_config['postgres_user'],
         password=postgres_config['postgres_password'],
@@ -139,109 +137,76 @@ def check_postgres_database_exists(database, postgres_config):
         port=postgres_config['postgres_port']
     )
 
+    query = generate_check_database_query(database=postgres_config['postgres_database'])
+
     try:
-        query = generate_check_database_query(database=database)
-        df = pd.read_sql_query(sql=query, con=engine)
-        if len(df) > 0:
+        connection.execute(query=query)
+        data = connection.fetchall()
+        if data[0][1] == postgres_config['postgres_database']:
             exists = True
-        elif len(df) == 0:
+        else:
             exists = False
     except:
         exists = False
 
-    print(f"database exists: {exists}")
+    connection.close()
 
     return exists
 
-def check_postgres_table_exists(table_name, postgres_config):
 
+def check_table_exists(connection, table_name):
     """
     Input:
+        connection | psycopg2 cursor | method of connecting to the database and executing queries
         table_name | string | name of the table
-        postgres_config | json | method of connecting to the table
     Output:
-        exits | bool | True if the table exists
+        exists | boolean | status of the existence of the database
     """
 
-    exists = None
-
-    engine = establish_postgres_engine(
-        database=postgres_config['postgres_database'],
-        user=postgres_config['postgres_user'],
-        password=postgres_config['postgres_password'],
-        host=postgres_config['postgres_host'],
-        port=postgres_config['postgres_port']
-    )
+    query = generate_check_table_query(table_name=table_name)
 
     try:
-        query = generate_check_table_query(table_name=table_name)
-        df = pd.read_sql_query(sql=query, con=engine)
-        if len(df) > 0:
+        connection.execute(query=query)
+        data = connection.fetchall()
+        if data[0][0] == True:
             exists = True
-        elif len(df) > 0:
+        else:
             exists = False
     except:
         exists = False
 
-    print(f"table exists: {exists}")
-
     return exists
 
-
-# Execute no return queries
-def create_table(table_schema, table_name, postgres_config):
-    """
-	Input:
-		table_schema | json | key value pairing of column name and type
-		table_name | string | name of the table
-		postgres_config | json | method of connecting to the database
-	Output:
-		None
-	"""
-
-    postgres_connection = establish_postgres_connection(
-        database=postgres_config["postgres_database"],
-        user=postgres_config['postgres_user'],
-        password=postgres_config['postgres_password'],
-        host=postgres_config['postgres_host'],
-        port=postgres_config['postgres_port']
-    )
-
-    query = generate_create_table_query(table_schema, table_name)
-
-    postgres_connection.execute(query)
-
-    return None
-
-def create_database(database, postgres_connection):
-    """
-	Input:
-		database | string | name of the database
-		postgres_connection | psycopg2 cursor | method of connecting to the database
-	Output:
-		None
-	"""
-
-    query = generate_create_database_query(database)
-
-    postgres_connection.execute(query)
-
-    return None
-
-def validate_postgres_database_and_table(database, table_name, postgres_config, table_schemas):
-
+# none returns
+def create_table(connection, table_name, table_schema):
     """
     Input:
-        database | string | name of the database
+        connection | psycopg2 cursor | method of connecting to the database and executing queries
         table_name | string | name of the table
-        postgres_config | json |  method of connecting to the database/table
-        table_schemas: | json | method of programmatically creating a table with a particular schema
+        table_schema | json | key value pair for all column names and types
+    output:
+        None
+    """
+
+    query = generate_create_table_query(
+        table_schema=table_schema,
+        table_name=table_name,
+    )
+
+    connection.execute(query)
+
+    return None
+
+
+def create_database(postgres_config):
+    """
+    Input:
+        postgres_config | json | key value pairs needed to query the specified postgres
     Output:
         None
     """
 
-
-    base_connection = establish_postgres_connection(
+    connection = establish_postgres_connection(
         database="postgres",
         user=postgres_config['postgres_user'],
         password=postgres_config['postgres_password'],
@@ -249,86 +214,125 @@ def validate_postgres_database_and_table(database, table_name, postgres_config, 
         port=postgres_config['postgres_port']
     )
 
-    database_check = check_postgres_database_exists(
-        database=database,
-        postgres_config=postgres_config
-    )
+    query = generate_create_database_query(database=postgres_config["postgres_database"])
 
-    table_check = check_postgres_table_exists(
-        table_name=table_name,
-        postgres_config=postgres_config,
-    )
+    connection.execute(query)
 
-    if database_check == False:
-
-        print(f"creating database: {postgres_config['postgres_database']}")
-        print(f"creating table: {postgres_config['postgres_table']}")
-
-        # create the database
-        create_database(
-            database=postgres_config['postgres_database'],
-            postgres_connection=base_connection)
-
-        # create the stable with a known schema
-        create_table(
-            table_schema=table_schemas['ohlc'],
-            table_name=postgres_config['postgres_table'],
-            postgres_config=postgres_config
-        )
-
-    elif database_check == True and table_check == False:
-
-        print(f"database: {postgres_config['postgres_database']} exists")
-        print(f"creating table: {postgres_config['postgres_table']}")
-
-        # create the table with a known schema
-        create_table(
-            table_schema=table_schemas['ohlc'],
-            table_name=postgres_config['postgres_table'],
-            postgres_config=postgres_config
-        )
-
-    else:
-        print(f"database: {postgres_config['postgres_database']} | status: exist")
-        print(f"table: {postgres_config['postgres_table']} | status: exist")
-        pass
+    connection.close()
 
     return None
 
-def write_to_postgres(data, table_name, engine):
+# misc
+def validate(postgres_config, table_schema):
     """
-	Input:
-		data | pandas dataframe | psycopg2 cursor | method of connecting to the database
-		table_name | string | name of the table
-		engine | str | method of connecting to the database used with pandas
-	Output:
-		None
-	"""
+    Input:
+        postgres_config | json | key value pairs needed to query the specified postgres
+        table_schema | json | key value pair for all column names and types
+    Output:
+        connection | psycopg2 cursor | method of connecting to the database and executing queries
+    """
 
-    if data is not None:
-        print(f"Inserting {len(data)} records into {table_name}")
-        data.to_sql(name=table_name, con=engine, if_exists='append', index=False)
+    db_status = check_database_exists(postgres_config=postgres_config)
+
+    if db_status == True:
+
+        print(f"Database {postgres_config['postgres_database']} exists")
+
+        connection = establish_postgres_connection(
+            database=postgres_config['postgres_database'],
+            user=postgres_config['postgres_user'],
+            password=postgres_config['postgres_password'],
+            host=postgres_config['postgres_host'],
+            port=postgres_config['postgres_port']
+        )
+
+        table_status = check_table_exists(
+            connection=connection,
+            table_name=postgres_config['postgres_table']
+        )
+
+        if table_status == True:
+            print(f"Table {postgres_config['postgres_table']} exists")
+            pass
+        else:
+            print("Table does not exist")
+            print(f"Creating table: {postgres_config['postgres_table']}")
+            create_table(
+                connection=connection,
+                table_schema=table_schema,
+                table_name=postgres_config['postgres_table']
+            )
+            print(f"Created table: {postgres_config['postgres_database']}")
+
     else:
-        print(f"No records inserted into {table_name}")
+        print(f"Database {postgres_config['postgres_database']} does not exists")
+        print(f"Table {postgres_config['postgres_table']} does not exists")
+
+        create_database(postgres_config=postgres_config)
+        print(f"Created database: {postgres_config['postgres_database']}")
+
+        connection = establish_postgres_connection(
+            database=postgres_config['postgres_database'],
+            user=postgres_config['postgres_user'],
+            password=postgres_config['postgres_password'],
+            host=postgres_config['postgres_host'],
+            port=postgres_config['postgres_port']
+        )
+
+        create_table(
+            connection=connection,
+            table_schema=table_schema,
+            table_name=postgres_config['postgres_table']
+        )
+        print(f"Created table: {postgres_config['postgres_table']}")
+
+    return connection
+
+
+def insert_records(data, table_name, connection):
+    """
+    Input:
+        data | pd.DataFrame | dataframe of records to insert
+        table_name | string | name of the table
+        connection | psycopg2 cursor | method of connecting to the database
+    Output:
+        None
+    """
+    columns = data.columns.tolist()
+    column_names = ", ".join(columns)
+    values = ", ".join(['%s'] * len(columns))
+
+    query = generate_insert_query(table_name, column_names, values)
+
+    data_to_insert = [tuple(row) for row in data.values]
+
+    connection.executemany(query, data_to_insert)
+
+    print(f"Inserted {len(data)} records into {table_name}")
 
     return None
 
 
-# Execute record return queries
-def query_postgres_last_record(engine, table_name):
+def query_postgres_last_record(connection, table_name):
+    """
+    Input:
+        connection | psycopg2 cursor | method of connecting to the database
+        table_name | string | name of the table
+    Output:
+        laste_date | string | date of last record in the format "YYYY-MM-DD HH:MM:SS"
+    """
+    query = generate_last_record_query(
+        column_of_interest="date",
+        table_name=table_name
+    )
 
-	query = generate_last_record_query(
-		column_of_interest="date",
-		table_name=table_name
-	)
+    try:
+        connection.execute(query)
+        last_record = connection.fetchone()
+        last_date = str(last_record[1])
+    except:
+        print(f"No records at table: {table_name}")
+        last_date = "2018-01-01 00:00:00"
+        print(f"will assume last date of: {last_date}")
 
-	try:
-		last_record_df = pd.read_sql_query(sql=query, con=engine)
-		last_record = str(last_record_df.iloc[0]['date'])
-		last_date = last_record.split("+")[0]
-	except:
-		print(f"table: {table_name} has not records")
-		last_date = "2018-01-01 00:00:00"
-		print(f"will assume last date of: {last_date}")
-
-	return last_date
+    return last_date
